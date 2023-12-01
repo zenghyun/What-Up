98c5f49098fa39a1159b39c1f0edc9c2401aedbf
zenghyun <pipo36@naver.com>
2023-12-01 21:26:29 +0900

    Update annotation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
 // 웹소켓 세팅
 const { createServer } = require("http");
-const app = require("./app");
+const appServer = require("./app");
 const { Server } = require("socket.io");
+// dotenv => node.js에서 환경 변수를 좀 더 효과적으로 관리하기 위해 사용 
 require("dotenv").config();
 
-const httpServer = createServer(app);
+const httpServer = createServer(appServer);
 const io = new Server(httpServer, {
   cors: {
     origin: "http://127.0.0.1:5173",
