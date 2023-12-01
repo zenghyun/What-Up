fed69ab464e1b47d0947af8fc0bdf2c5e513e7fa
zenghyun <pipo36@naver.com>
2023-12-01 21:27:25 +0900

    Save user & message

--- backend/src/utils/io.ts
+++ backend/src/utils/io.ts
@@ -1,10 +1,37 @@
+const getUserController = require("../Controllers/user.controller");
+const getChatController = require("../Controllers/chat.controller");
+
 module.exports = function (io) {
   // 연결된 사람의 정보를 매개변수로 넘김
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
 
     socket.on("login", async (userName, cb) => {
-      console.log(userName);
+      // 유저정보를 저장
+      try {
+        const user = await getUserController.saveUser(userName, socket.id);
+        const welcomeMessage = {
+          chat: `${user.name}님이 방에 입장하였습니다.`,
+          user: { id: null, name: "system" },
+        };
+        io.emit("message", welcomeMessage);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        // 유저 찾기 socket.id
+        const user = await getUserController.checkUser(socket.id);
+        // 메세지 저장 (유저)
+        const newMessage = await getChatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ ok: true });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
     });
 
     socket.on("disconnect", () => {
